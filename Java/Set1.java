05e16b3a7cce87ec2431c2ce5c73f1d5b6a6dcc3
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-03-13 21:07:04 -0700

    Started Set1

--- Java/Set1.java
+++ Java/Set1.java
@@ -1,9 +1,11 @@
 public class Set1 {
 	public static void main(String [] args) {
 		String text = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-		for (int i = 0; i < text.length(); i += 2) {
-			
-	public static int from_hex_to_decimal(String hex) {
+		for (int i = 0; i < text.length(); i++)
+			System.out.print(from_hex_to_decimal(text.charAt(i)));
+	}
+	
+	public static int from_hex_to_decimal(char hex) {
 		if ('0' <= hex && hex <= '9')
 			return hex - '0';
 		if (hex >= 'A' && hex <= 'Z')
