e53e06b5e343609457ad4be5e036dd28b57857b6
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-03-14 04:27:45 +0000

    Need Testing

--- Java/Set1.java
+++ Java/Set1.java
@@ -1,8 +1,8 @@
 public class Set1 {
 	public static void main(String [] args) {
 		String text = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-		for (int i = 0; i < text.length(); i++)
-			System.out.print(from_hex_to_decimal(text.charAt(i)));
+		for (int i = 0; i < text.length(); i += 2)
+			System.out.print(from_hex_to_decimal(toHex(text.substring(i, i+2))));
 	}
 	
 	public static int from_hex_to_decimal(char hex) {
@@ -12,5 +12,8 @@ public class Set1 {
 			return hex - 'A' + 10;
 		return hex - 'a' + 10;
 	 }
+	
+	public static String toHex(String arg) {
+		return String.format("%040x", new BigInteger(1, arg.getBytes()));
+	}
 }
-
